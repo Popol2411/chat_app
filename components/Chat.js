861443ca5fd31939e2f603cbf7a580d705bee677
Popol2411 <96198379+Popol2411@users.noreply.github.com>
2022-06-03 19:44:32 +0200

    GiftedChat added

--- components/Chat.js
+++ components/Chat.js
@@ -1,29 +1,64 @@
 import React from 'react';
 import { View, StyleSheet, Pressable, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
-  render() {
 
-    let bgColor = this.props.route.params.bgColor;
-
-    return (
-      <View style={styles.container}>
-        <View
-          style={{ backgroundColor: bgColor, width: '100%', height: '100%' }}
-        ></View>
-        <Pressable
-        onPress={() => this.props.navigation.navigate('Start')}>
-          <Text style={styles.startButton}>Go To Start</Text>
-        </Pressable>
-      </View>
-    );
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
   }
 
   componentDidMount() {
+
     let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    });
+  
   } 
-}
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+render() {
+
+  let bgColor = this.props.route.params.bgColor;
+
+  return (
+    <View style={styles.container}>
+      <View style={{ backgroundColor: bgColor, width: '100%', height: '100%' }} >        
+      <GiftedChat
+        style={styles.giftedChat}
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{_id: 1, }}/>
+      <Pressable
+      onPress={() => this.props.navigation.navigate('Start')}>
+        <Text style={styles.startButton}>Go To Start</Text>
+      </Pressable>
+      </View>
+    </View>
+  );
+}}
 
 const styles = StyleSheet.create({
   container: {
@@ -33,8 +68,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   startButton: {
-    paddingTop: 10,
+    paddingTop: 35,
     fontSize: 16,
     minHeight: 100,
+    alignSelf: 'center',
+  },
+  giftedChat: {
+    color: '#000',
   },
 });
